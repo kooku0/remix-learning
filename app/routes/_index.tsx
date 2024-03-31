f93ecea46401f7c26abc1ee6436217fc42d51d06
kooku0 <koomg9599@gmail.com>
2024-03-31 15:58:39 +0900

    feat: public assets

--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -1,7 +1,5 @@
 import type { MetaFunction } from '@remix-run/node';
 
-import bird from "../../public/asset/images/bird.png";
-
 export const meta: MetaFunction = () => {
   return [{ title: 'New Remix App' }, { name: 'description', content: 'Welcome to Remix!' }];
 };
@@ -10,7 +8,7 @@ export default function Index() {
   return (
     <div style={{ fontFamily: 'system-ui, sans-serif', lineHeight: '1.8' }}>
       <h1>Welcome to Remix</h1>
-      <img src={bird} width={200} height={200} />
+      <img src="/assets/images/bird.png" width={200} height={200} />
       <ul>
         <li>
           <a href="https://remix.run/tutorials/blog" rel="noreferrer" target="_blank">
